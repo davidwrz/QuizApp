24b52c91b6a113ca9f86cae7a7d6e95d11bdfa7a
Dawid Wrzos <wrzosdawid@wp.pl>
2023-06-14 20:31:18 +0200

    Change primary type in migration scripts to avoid null

--- src/main/resources/db/migration/V1__create_quiz_answers.sql
+++ src/main/resources/db/migration/V1__create_quiz_answers.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS question (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     content VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS answer (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     content VARCHAR(255) NOT NULL,
     correct BOOLEAN NOT NULL,
     question_id INTEGER,
