298f24b01c49197a1349912cdba21c3457f04762
Dawid Wrzos <79601947+davidwrz@users.noreply.github.com>
2023-11-02 12:42:00 +0100

    Rename api path of question (#6)

--- src/main/java/au/davidwrz/quizapp/modules/question/answer/infrastracture/web/AnswerQuestionController.java
+++ src/main/java/au/davidwrz/quizapp/modules/question/answer/infrastracture/web/AnswerQuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/questions")
+@RequestMapping("api/v1/question")
 class AnswerQuestionController {
 
     private final AnswerQuestionFacade service;
